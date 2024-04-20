96814375500eb3b6a71ff485d8a50bfbd51ee1aa
Dinis Vieira <dinisvieira@outlook.com>
2024-04-20 16:04:00 +0100

    Added initial code that displays issue

---
+++ MauiAppGridCenteredWidthBounds/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppGridCenteredWidthBounds
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MauiAppGridCenteredWidthBounds/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppGridCenteredWidthBounds
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
